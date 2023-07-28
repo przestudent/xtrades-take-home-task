8274d456217637b5227f0026098e1f5245d31bd7
3 <przemekpalac@student.agh.edu.pl>
2023-07-28 21:50:48 +0200

    Dots

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  styles: [
+    `
+      :host {
+        padding-bottom: 5rem;
+      }
+    `,
+  ],
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
